c3de19cbf2d9ca0ab33a9feca781b39d8459861c
Nikitha Bhavani <nikithabhavani966@gmail.com>
2024-06-08 00:43:18 +0530

    added card details

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const options = {
 };
 
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+	.then(response => response.json())
+	.then(response => {
+		console.log(response)
+		cloud_pct.innerHTML = response.cloud_pct
+		temp.innerHTML = response.temp
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		max_temp.innerHTML = response.max_temp
+		min_temp.innerHTML = response.min_temp
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+		wind_degrees.innerHTML = response.wind_degrees
+		wind_speed.innerHTML = response.wind_speed
+	})
+	.catch(err => console.error(err));
