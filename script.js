1e81672896787f3daa3ea4f6bb36a9bfbd7805e3
Vinay Malyala <srivinay1432@gmail.com>
2024-06-09 13:11:29 +0530

    added weather of other places

--- script.js
+++ script.js
@@ -32,3 +32,90 @@ submit.addEventListener("click", (e) => {
 })
 
 getWeather("Delhi")
+
+
+const shanghaiWeather = () => {
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Shanghai', options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			shanghai_cloud_pct.innerHTML = response.cloud_pct
+			shanghai_temp.innerHTML = response.temp
+			shanghai_feels_like.innerHTML = response.feels_like
+			shanghai_humidity.innerHTML = response.humidity
+			shanghai_max_temp.innerHTML = response.max_temp
+			shanghai_min_temp.innerHTML = response.min_temp
+			shanghai_sunrise.innerHTML = response.sunrise
+			shanghai_sunset.innerHTML = response.sunset
+			shanghai_wind_degrees.innerHTML = response.wind_degrees
+			shanghai_wind_speed.innerHTML = response.wind_speed
+		})
+		.catch(err => console.error(err));
+}
+
+shanghaiWeather()
+
+
+const bostonWeather = () => {
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Boston', options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			boston_cloud_pct.innerHTML = response.cloud_pct
+			boston_temp.innerHTML = response.temp
+			boston_feels_like.innerHTML = response.feels_like
+			boston_humidity.innerHTML = response.humidity
+			boston_max_temp.innerHTML = response.max_temp
+			boston_min_temp.innerHTML = response.min_temp
+			boston_sunrise.innerHTML = response.sunrise
+			boston_sunset.innerHTML = response.sunset
+			boston_wind_degrees.innerHTML = response.wind_degrees
+			boston_wind_speed.innerHTML = response.wind_speed
+		})
+		.catch(err => console.error(err));
+}
+
+bostonWeather()
+
+
+const newYorkWeather = () => {
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=New York', options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			newYork_cloud_pct.innerHTML = response.cloud_pct
+			newYork_temp.innerHTML = response.temp
+			newYork_feels_like.innerHTML = response.feels_like
+			newYork_humidity.innerHTML = response.humidity
+			newYork_max_temp.innerHTML = response.max_temp
+			newYork_min_temp.innerHTML = response.min_temp
+			newYork_sunrise.innerHTML = response.sunrise
+			newYork_sunset.innerHTML = response.sunset
+			newYork_wind_degrees.innerHTML = response.wind_degrees
+			newYork_wind_speed.innerHTML = response.wind_speed
+		})
+		.catch(err => console.error(err));
+}
+
+newYorkWeather()
+
+const seoulWeather = () => {
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seoul', options)
+		.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			seoul_cloud_pct.innerHTML = response.cloud_pct
+			seoul_temp.innerHTML = response.temp
+			seoul_feels_like.innerHTML = response.feels_like
+			seoul_humidity.innerHTML = response.humidity
+			seoul_max_temp.innerHTML = response.max_temp
+			seoul_min_temp.innerHTML = response.min_temp
+			seoul_sunrise.innerHTML = response.sunrise
+			seoul_sunset.innerHTML = response.sunset
+			seoul_wind_degrees.innerHTML = response.wind_degrees
+			seoul_wind_speed.innerHTML = response.wind_speed
+		})
+		.catch(err => console.error(err));
+}
+
+seoulWeather()
